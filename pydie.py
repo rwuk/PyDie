cacf28a26a975dce9938703f82f9a4a182b83e7b
Syn <syn74x@gmx.com>
2017-01-10 12:51:03 +0000

    Update pydie.py

--- pydie.py
+++ pydie.py
@@ -1,7 +1,10 @@
 import sys, os, random					#PyDie - A Python Dice Box with log file
 
-def header():
+def header():							#Graphical Header
 	print(" ---------- PyDie ----------\n ")
+
+def cont():								#Function to continue
+	input("Press Return to continue...")
 
 def dice(a, b,):						#Rolls The Dice
 	total = 0
@@ -11,8 +14,13 @@ def dice(a, b,):						#Rolls The Dice
 		total = total + roll
 	return total
 
-def cont():								#Function to continue
-	input("Press Return to continue...")
+def int_check(a):						#Check user input is an integer
+	check = a.isdigit()
+	if check == True:
+		return int(a)
+	else:
+		fail()
+		menu()
 
 def clear_screen():						#Clear Screen function - OS Specific
 	if sys.platform == 'linux2' or sys.platform == 'linux':
@@ -29,13 +37,14 @@ def fail():								#Fail Checker
 	cont()
 	menu()
 
-def int_check(a):						#Check user input is an integer
-	check = a.isdigit()
-	if check == True:
-		return int(a)
-	else:
-		fail()
-		menu()
+def view():								#Access the log file
+	clear_screen()
+	header()
+	log.seek(0)
+	print(log.read())
+	cont()
+	clear_screen()
+	menu()
 
 def erase():
 	clear_screen()						#Wipe the log file
@@ -55,14 +64,24 @@ def erase():
 	else:
 		fail()
 
-def view():								#Access the log file
+def menu():								#Menu System
 	clear_screen()
 	header()
-	log.seek(0)
-	print(log.read())
-	cont()
-	clear_screen()
-	menu()
+	print("1:) Roll some dice.")
+	print("2:) View the log.")
+	print("3:) Wipe the log.")
+	print("4:) Exit PyDie.")
+	response = str(input("\nWhat would you like to do? > "))
+	if response == "1":
+		engine()
+	elif response == "2":
+		view()
+	elif response == "3":
+		erase()
+	elif response == "4":
+		close()
+	else:
+		fail()
 
 def engine():							#Main program loop
 	clear_screen()
@@ -83,25 +102,6 @@ def engine():							#Main program loop
 	log.write("\n")
 	cont()
 	menu()
-
-def menu():								#Main Menu
-	clear_screen()
-	header()
-	print("1:) Roll some dice.")
-	print("2:) View the log.")
-	print("3:) Wipe the log.")
-	print("4:) Exit PyDie.")
-	response = str(input("\nWhat would you like to do? > "))
-	if response == "1":
-		engine()
-	elif response == "2":
-		view()
-	elif response == "3":
-		erase()
-	elif response == "4":
-		close()
-	else:
-		fail()
 
 def close():							#Close PyDie
 	clear_screen()
